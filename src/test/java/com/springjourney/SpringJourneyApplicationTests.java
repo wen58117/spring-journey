4be27a33228625fac79f15211ec85f44ae06100a
xuwenjie <wen_58117@163.com>
2025-06-06 10:41:11 +0800

    init

---
+++ src/test/java/com/springjourney/SpringJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
