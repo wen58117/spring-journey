9856ef075335870e5e5ba94c705a387e44e76dc5
xuwenjie <wen_58117@163.com>
2025-06-06 17:26:16 +0800

    feat: 日志模板记录

--- src/main/java/com/springJourney/service/MyServiceInit.java
+++ src/main/java/com/springJourney/service/MyServiceInit.java
@@ -1,17 +1,24 @@
 package com.springJourney.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.annotation.PostConstruct;
 
+@Slf4j
 public class MyServiceInit {
+    private static final Logger logger = LoggerFactory.getLogger(MyServiceInit.class);
+
     private String message;
 
     @PostConstruct
     public void init() {
         this.message = "Initialized!";
-        System.out.println("MyServiceInit initialized: " + message);
+        logger.info("MyServiceInit initialized: {}", message);
     }
 
     public void sayHello() {
-        System.out.println(message);
+        log.info(message);
     }
 }
